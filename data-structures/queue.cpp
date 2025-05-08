c504d4f4d81e3e963f64a71475e1cd75868c4b9a
AKUZWE-Sheja <edwigeakuzwe6@gmail.com>
2025-05-08 14:36:10 +0200

    dsa.v1~

---
+++ data-structures/queue.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+
+using namespace std;
+
+// Node structure
+template <typename T>
+struct Node
+{
+    T data;
+    Node *next;
+
+    Node(T val) : data(val), next(nullptr) {}
+};
+
+// Queue class
+template <typename T>
+class Queue
+{
+private:
+    Node<T> *front;
+    Node<T> *rear;
+
+public:
+    Queue() : front(nullptr), rear(nullptr) {}
+
+    // Enqueue (insert) an element at the rear of the queue
+    void enqueue(T val)
+    {
+        Node<T> *newNode = new Node<T>(val);
+        if (rear == nullptr)
+        {
+            front = rear = newNode;
+        }
+        else
+        {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    // Dequeue (remove) an element from the front of the queue
+    void dequeue()
+    {
+        if (front == nullptr)
+        {
+            cout << "Queue is empty, cannot dequeue.\n";
+            return;
+        }
+        Node<T> *temp = front;
+        front = front->next;
+        if (front == nullptr)
+        {
+            rear = nullptr;
+        }
+        delete temp;
+    }
+
+    // Get the front element of the queue
+    T getFront()
+    {
+        if (front == nullptr)
+        {
+            cout << "Queue is empty.\n";
+            return T(); // Return default value of T
+        }
+        return front->data;
+    }
+
+    // Check if the queue is empty
+    bool isEmpty()
+    {
+        return front == nullptr;
+    }
+
+    // Print all elements of the queue
+    void printQueue()
+    {
+        if (front == nullptr)
+        {
+            cout << "Queue is empty.\n";
+            return;
+        }
+        Node<T> *current = front;
+        while (current != nullptr)
+        {
+            cout << current->data << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    // Destructor to free the allocated memory
+    ~Queue()
+    {
+        while (front != nullptr)
+        {
+            Node<T> *temp = front;
+            front = front->next;
+            delete temp;
+        }
+    }
+};
+
+// Main function
+// int main() {
+//    Queue<int> q;
+
+//    q.enqueue(10);
+//    q.enqueue(20);
+//    q.enqueue(30);
+
+//    cout << "Front element: " << q.getFront() << endl;
+
+//    cout << "Queue elements: ";
+//    q.printQueue();
+
+//    q.dequeue();
+//    cout << "Front element after dequeue: " << q.getFront() << endl;
+
+//    cout << "Queue elements after dequeue: ";
+//    q.printQueue();
+
+//    q.dequeue();
+//    q.dequeue();
+//    q.dequeue();  // Attempt to dequeue from an empty queue
+
+//    if (q.isEmpty()) {
+//        cout << "Queue is empty.\n";
+//    }
+
+//    return 0;
+// }
